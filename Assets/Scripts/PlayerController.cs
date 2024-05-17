146a8859512105ab13449fa9ee7d7e79edefb597
Juttacci <guy.hazoume@epitech.eu>
2024-05-17 01:01:44 +0100

    Add : Health System
    Set up a basic health system and create heath collectible using triggers

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,24 +13,37 @@ public class PlayerController : MonoBehaviour
 
     Vector2 move;
 
+    public int maxHealth = 5;
+
+    int currentHealth = 1;
+
+    public float characterSpeed = 3.0f;
+
     void Start()
     {
         MoveAction.Enable();
-
         rigidbody2d = GetComponent<Rigidbody2D>();
-    }
 
+        //currentHealth = maxHealth;
+    }
     
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
 
-        Debug.Log(move);
+        //Debug.Log(move);
+    }
+
+    public void ChangeHealth (int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 
     void FixedUpdate()
     {
-        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        Vector2 position = (Vector2)rigidbody2d.position + move * characterSpeed * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
