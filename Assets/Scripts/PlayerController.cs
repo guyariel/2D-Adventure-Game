181408e62d594a333b457a677fd7053415b59630
Juttacci <guy.hazoume@epitech.eu>
2024-05-24 17:06:39 +0100

    Update : PlayerController and UIHandler scripts
    Complete the UIHandler system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -66,6 +66,7 @@ public class PlayerController : MonoBehaviour
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
 
-        Debug.Log(currentHealth + "/" + maxHealth);
+        UIHandler.instance.SetHealthValue(currentHealth / (float)maxHealth);
+
     }
 }
